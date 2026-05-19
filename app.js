d1903b87e707e65485b4ba80962faa1ee93f89de
shivansh-mehra-01 <mehrashiv8889@gmail.com>
2026-05-19 11:25:34 +0530

    Fix session cookie security for Render deployment

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ const sessionOptions = {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: false,
     },
 };
 
